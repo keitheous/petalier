76e8bbca90c8f4f4edd30729b9aeff66e83104da
keitheous <keithchongwy@gmail.com>
2018-05-30 21:06:36 +1000

    Layout for Scroll

---
+++ javascripts/app.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+  var expendNavBar = function(){
+    $('#nav-icon').removeClass('fa-bars').addClass('fa-times');
+    $('.nav-items').show()
+  }
+
+  var collapseNavBar = function(){
+    $('#nav-icon').addClass('fa-bars').removeClass('fa-times');
+    $('.nav-items').hide();
+  }
+
+  $('#nav-icon').on('click', function(){
+      $(this).hasClass("fa-bars") ? expendNavBar() : collapseNavBar()
+  })
+});
